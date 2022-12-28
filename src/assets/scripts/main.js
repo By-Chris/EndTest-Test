983995db86f16ef22a4d60ec196f1d4304f5d55a
By-Chris <121507127+By-Chris@users.noreply.github.com>
2022-12-28 13:37:53 -0500

    Adding accordion to home page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,22 @@
 // Javascript goes here
 console.log('up to zero!');
 
+//accordian script
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-jQuery(function($){
-    console.log('hi');
-  //   $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
-  //   $("#phone").mask("(999) 999-9999");
-  //   $("#tin").mask("99-9999999");
-  //   $("#ssn").mask("999-99-9999");
-    $('.telephone').mask("(999)-999-9999");
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
   });
+}
